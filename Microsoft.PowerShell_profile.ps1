5dbaf0b4fbabc034b5cb9fb4e1229f241521ba44
Paul Travis <paul.travis@voicefoundry.com>
2021-10-25 23:02:08 -0400

    Added Powershell Profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,60 @@
+function cdls {
+
+  param (
+    [string]$path = $( Read-Host "> ")
+  )
+  
+  cd $path
+  wsl ls
+
+}
+
+function mkcd {
+
+  param (
+    [string]$path = $( Read-Host "> ")
+  )
+
+  mkdir $path
+  cd $path
+
+}
+
+function psrc {
+
+  nvim $PROFILE 
+
+}
+
+function Prompt {
+
+  $gitBranch = $( git rev-parse --abbrev-ref HEAD ) 
+  $ESC = [char]27
+  $gitStatus = $( git status -bs | wsl grep -o '\\[[^]]*]' )
+
+  if ($gitStatus -eq "" -or $gitStatus -eq $null) {
+
+    $gitStatus = "[Up To Date]"
+
+  }
+
+  if ($gitBranch -eq $null -or $gitBranch -eq "") {
+
+    '' + $(Get-Location) + '> ' 
+
+  } else {
+
+    $ESC + '[31m' + $gitBranch + " | " + $gitStatus + "$ESC[0m`n" + $(Get-Location) + '> '
+
+  }
+
+}
+
+function slm {
+
+  cd "c:\Users\3364324\code\slm"
+
+}
+
+New-Alias vi nvim
+Set-PSReadLineOption -EditMode Vi
