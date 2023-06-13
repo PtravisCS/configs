00418c82dd66384497f4f08f7fea368d5c782c19
travisp <paul.travis@michigansugar.com>
2023-06-13 17:35:14 -0400

    Updated prompt to bring in line with my BASH prompt. Added aliases. Re-arranged file. Other minor changes.

--- config_files/Microsoft.PowerShell_profile.ps1
+++ config_files/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
+
+# Functions
 function cdls {
 
   param (
@@ -20,55 +22,75 @@ function mkcd {
 
 }
 
-function psrc {
+# Prompt Stuff
+function gitBranch {
 
-  nvim $PROFILE 
+  $gitBranch = $(git rev-parse --abbrev-ref HEAD 2> $null)
+  return $gitBranch
 
 }
 
-<#
-function Prompt {
+function gitStatus {
 
-  $gitBranch = $( git rev-parse --abbrev-ref HEAD ) 
-  $ESC = [char]27
-  $gitStatus = $( git status -bs | wsl grep -o '\\[[^]]*]' )
+  $gitBranch = $(gitBranch)
 
-  if ($gitStatus -eq "" -or $gitStatus -eq $null) {
+  if ($gitBranch) {
+    git fetch --multiple --no-tags 2> $null
+    $behind = $(git rev-list --count HEAD..`@`{u`} 2> $null)
+    $ahead = $(git rev-list --count `@`{u`}..HEAD 2> $null)
+    $changed = $(git status --porcelain 2> $null)
 
-    $gitStatus = "[Up To Date]"
+    if ($changed) {
+      $changed = $changed.Split([Environment]::NewLine)
+    }
 
+    if ([int]$behind -eq 0 -and $ahead -eq 0 ) {
+      $gitstatus = '[=]'
+    } elseif ([int]$behind -ne 0 -and $ahead -eq 0) {
+      $gitstatus = "[b: $behind]"
+    } elseif ([int]$ahead -ne 0 -and $behind -eq 0) {
+      $gitstatus = "[a: $ahead]"
+    } else {
+      $gitstatus = "[b: $behind | a: $ahead]"
+    }
+
+    if ($changed.Length -gt 0) { 
+      $num_changes = $changed.Length
+      $change_string = "[c: $num_changes]"
+    }
+
+    "`u{256D}`u{2574}`u{E0A0} $gitBranch | $gitstatus$change_string`n`u{2570}`u{2574}"
   }
-
-  if ($gitBranch -eq $null -or $gitBranch -eq "") {
-
-    '' + $(Get-Location) + '> ' 
-
-  } else {
-
-    $ESC + '[31m' + $gitBranch + " | " + $gitStatus + "$ESC[0m`n" + $(Get-Location) + '> '
-
-  }
-
 }
-#>
 
-function potato {
+function prompt {
 
-  echo "Potato"
+  "$(
+  Write-Host "$(gitStatus)" -NoNewLine -ForegroundColor red;
+  Write-Host "$([Environment]::UserName)@$(hostname)" -NoNewLine -ForegroundColor green;
+  Write-Host ":" -NoNewLine -ForegroundColor white;
+  Write-Host "$(Get-Location)" -NoNewLine -ForegroundColor blue;
+  )> "
 
 }
 
-function edit_profile {
+# Alias Functions
+function s { git status }
+function add { git add . }
+function c { git commit }
+function p { git push }
+function d { git d }
+function psrc { nvim $PROFILE }
+function potato { echo "Potato" }
+function edit_profile { nvim $PROFILE }
 
-  nvim $PROFILE
+# Aliases
+New-Alias -Name "vi" nvim
+New-Alias -Name "ls" dir
 
-}
-
-New-Alias vi nvim
+# Configurations and Modules
 Set-PSReadLineOption -EditMode Vi
 Import-Module -Name Terminal-Icons
 Import-Module -Name posh-git
 
-$GitPromptSettings.DefaultPromptWriteStatusFirst = $true
-$GitPromptSettings.DefaultPromptPath.Text = "`n" + $GitPromptSettings.DefaultPromptPath.Text 
 
