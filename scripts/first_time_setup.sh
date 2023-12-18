2be68da4f87b500380e9214cd96e734b1eb4d1c4
travisp <paul.travis@michigansugar.com>
2023-12-18 16:11:39 -0500

    Updated setup script to be a little more silent.

--- scripts/first_time_setup.sh
+++ scripts/first_time_setup.sh
@@ -3,99 +3,110 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install apache2
-sudo apt-get install apache2-dev
-sudo apt-get install aptitude
-sudo apt-get install build-essential
-sudo apt-get install cargo
-sudo apt-get install catimg
-sudo apt-get install clisp
-sudo apt-get install composer
-sudo apt-get install cpanminus
-sudo apt-get install curl
-sudo apt-get install docker
-sudo apt-get install dos2unix
-sudo apt-get install evolution
-sudo apt-get install evolution-ews
-sudo apt-get install fd-find
-sudo apt-get install feh
-sudo apt-get install ffmpeg
-sudo apt-get install flake8
-sudo apt-get install gdal-bin
-sudo apt-get install gforth
-sudo apt-get install gimp
-sudo apt-get install git
-sudo apt-get install gnome-tweaks
-sudo apt-get install highlight
-sudo apt-get install imagemagick
-sudo apt-get install imagemagick-6.q16
-sudo apt-get install libperl-critic-perl 
-sudo apt-get install lua5.4
-sudo apt-get install inkscape
-sudo apt-get install keepassx
+progs=('apache2' 
+  'apache2-dev' 
+  'aptitude' 
+  'build-essential' 
+  'cargo' 
+	'catimg'
+	'clisp'
+	'composer'
+	'cpanminus'
+	'curl'
+	'docker'
+	'dos2unix'
+	'evolution'
+	'evolution-ews'
+	'fd-find'
+	'feh'
+	'ffmpeg'
+	'flake8'
+	'gldal-bin'
+	'gforth'
+  'gimp'
+  'git'
+  'gnome-tweaks'
+  'highlight'
+  'imagemagick'
+  'imagemagick-6.q16'
+  'libperl-critic-perl'
+  'lua5.4'
+  'inkscape'
+  'keepassx'
+  'microsoft-edge-stable'
+  'ncal'
+  'net-tools'
+  'nodejs'
+  'npm'
+  'openjdk-8-jre'
+  'openssh-server'
+  'perl-doc'
+  'php'
+	'php8.1-curl'
+	'php8.1-fpm'
+	'php8.1-gd'
+	'php8.1-mbstring'
+	'php8.1-mysql'
+	'php8.1-opcache'
+	'php8.1-pgsql'
+	'php8.1-soap'
+	'php8.1-sqlite3'
+	'php8.1-xml'
+	'php8.1-zip'
+	'php-bcmath'
+	'php-codesniffer'
+	'php-redis'
+	'php-ssh2'
+	'postgresql'
+	'powershell'
+	'python3-pip'
+	'python3-venv'
+	'qt5-qmake'
+	'qtbase5-dev'
+	'qtbase5-doc-html'
+	'qtbase5-examples'
+	'qtcreator'
+	'r-base'
+	'redis-server'
+	'rename'
+	'ripgrep'
+	'screen'
+	'shellcheck'
+	'sleuthkit'
+	'smbclient'
+	'solaar'
+	'spell'
+	'tmux'
+	'tree'
+	'vim'
+	'vlc'
+	'wajig'
+	'wl-clipboard'
+)
+
+for t in "${progs[@]}"; do
+  printf 'Installing %s\n' "$t"
+  sudo apt-get -qq install "$t"
+done
 
 # Required for Android Studio
-sudo apt-get install android-sdk lib32z1 libapr1 libapr1-dev libaprutil1-dev libbz2-1.0:i386 libc6:i386 libncurses5:i386 libstdc++6:i386
+printf 'Installing android studio and dependancies\n'
+sudo apt-get -qq install android-sdk lib32z1 libapr1 libapr1-dev libaprutil1-dev libbz2-1.0:i386 libc6:i386 libncurses5:i386 libstdc++6:i386
 
-sudo apt-get install microsoft-edge-stable
-sudo apt-get install ncal
-sudo apt-get install net-tools
-sudo apt-get install nodejs
-sudo apt-get install npm
-sudo apt-get install openjdk-8-jre
-sudo apt-get install openssh-server
-sudo apt-get install perl-doc
-sudo apt-get install php
-sudo apt-get install php8.1-curl
-sudo apt-get install php8.1-fpm
-sudo apt-get install php8.1-gd
-sudo apt-get install php8.1-mbstring
-sudo apt-get install php8.1-mysql
-sudo apt-get install php8.1-opcache
-sudo apt-get install php8.1-pgsql
-sudo apt-get install php8.1-soap
-sudo apt-get install php8.1-sqlite3
-sudo apt-get install php8.1-xml
-sudo apt-get install php8.1-zip
-sudo apt-get install php-bcmath
-sudo apt-get install php-codesniffer
-sudo apt-get install php-redis
-sudo apt-get install php-ssh2
-sudo apt-get install postgresql
-sudo apt-get install powershell
-sudo apt-get install python3-pip
-sudo apt-get install python3-venv
-sudo apt-get install qt5-qmake
-sudo apt-get install qtbase5-dev
-sudo apt-get install qtbase5-doc-html
-sudo apt-get install qtbase5-examples
-sudo apt-get install qtcreator
-sudo apt-get install r-base
-sudo apt-get install redis-server
-sudo apt-get install rename
-sudo apt-get install ripgrep
-sudo apt-get install screen
-sudo apt-get install shellcheck
-sudo apt-get install sleuthkit
-sudo apt-get install smbclient
-sudo apt-get install solaar
-sudo apt-get install spell
-sudo apt-get install tmux
-sudo apt-get install tree
-sudo apt-get install vim
-sudo apt-get install vlc
-sudo apt-get install wajig
-sudo apt-get install wl-clipboard
+printf "Do you wish to install NeoVim? "
+read -r yon
 
-# Install Neovim
-cd ~/ || return
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-tar -xvzf nvim-linux64.tar.gz
-cd nvim-linux64 || (printf 'Unable to CD into nvim-linux64' && exit)
-rsync -a ./bin/ /usr/bin/
-rsync -a ./lib/ /usr/lib/
-rsync -a ./share/ /usr/share/
-rsync -a ./man/ /usr/local/man
+if [[ "$yon" == 'y' ]]; then
+  # Install Neovim
+  cd ~/ || return
+  wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+  tar -xvzf nvim-linux64.tar.gz
+  cd nvim-linux64 || (printf 'Unable to CD into nvim-linux64' && exit)
+  sudo rsync -a ./bin/ /usr/bin/
+  sudo rsync -a ./lib/ /usr/lib/
+  sudo rsync -a ./share/ /usr/share/
+  sudo rsync -a ./man/ /usr/local/man
+fi
 
 sudo apt-get autoremove
 sudo apt-get install --fix-missing
