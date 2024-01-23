59e7d2d4601d2032373c6ddac37ba7dac3335e0c
travisp <paul.travis@michigansugar.com>
2024-01-23 10:17:49 -0500

    Created temp.php

--- scripts/first_time_setup.ps1
+++ scripts/first_time_setup.ps1
@@ -2,46 +2,53 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 refreshenv
 
-choco install audacity -y
-choco install catimg -y
-choco install clisp -y
-choco install composer -y
-choco install cpan -y
-choco install cpanminus -y
-choco install docker-cli -y
-# choco install fd-find -y
-# choco install ffmpeg -y
-# choco install flake8 -y
-choco install gforth -y
-# choco install gimp -y
-choco install git -y
-choco install greenshot -y
-choco install gsudo -y
-choco install hg -y
-# choco install imagemagick
-# choco install inkscape
-choco install jdk17 -y 
-choco install jdk8 -y
-choco install keypass -y
-# choco install libpar-packer-perl
-# choco install libperl-critic-perl
-choco install maven -y
-choco install microsoft-windows-terminal -y
-choco install msys2 -y
-choco install neovim -y
-choco install nvm -y
-# choco install php
-choco install postman -y
-choco install powershell-core -y
-choco install powertoys -y
-choco install python2 -y
-choco install python3 -y
-choco install rustup -y
-# choco install ripgrep -y
-choco install screentogif -y
-choco install typescript -y
-choco install vscode -y
-choco install wireshark -y
+[String[]]$progs = @(
+  'audacity',
+  '# catimg',
+  '# clisp',
+  'composer',
+  '# cpan',
+  '# cpanminus',
+  'docker-cli',
+  '# fd-find',
+  '# ffmpeg',
+  '# flake8',
+  'gforth',
+  '# gimp',
+  'git',
+  'greenshot',
+  'gsudo',
+  'hg',
+  '# imagemagick',
+  '# inkscape',
+  'jdk17 ',
+  'jdk8',
+  'keypass',
+  '# libpar-packer-perl',
+  '# libperl-critic-perl',
+  'maven',
+  'microsoft-windows-terminal',
+  'msys2',
+  'neovim',
+  'nvm',
+  '# php',
+  'postman',
+  'powershell-core',
+  'powertoys',
+  'python2',
+  'python3',
+  'rustup',
+  '# ripgrep',
+  'screentogif',
+  'typescript',
+  'vscode',
+  'wireshark'
+)
+
+foreach ($prog in $progs) {
+  Write-Host "Installing $prog"
+  choco install "$prog" -y
+}
 
 # Post Choco Installations
 refreshenv
